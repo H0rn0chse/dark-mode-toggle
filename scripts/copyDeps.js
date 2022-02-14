e6e914780eb9f9a2ffc39dbbf132097c6eeb3ba2
H0rn0chse <h0rn0chse@gmx.de>
2022-02-14 18:13:36 +0100

    Added github pages deploy workflow

---
+++ scripts/copyDeps.js
@@ -0,0 +1,41 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const projectDir = path.join(__dirname, "../");
+
+const dependencies = [[
+    "/dist", "/libs/dark-mode-toggle"
+], [
+    "/node_modules/lottie-web/build/", "/libs/lottie-web"
+], [
+    "/node_modules/wc-github-corners/dist", "/libs/wc-github-corners"
+]];
+
+// copy dependencies to gh-pages
+dependencies.forEach((dep) => {
+    const pathFrom = path.join(projectDir, dep[0]);
+    const pathTo = path.join(projectDir, "gh-pages", dep[1]);
+    copyRecursiveSync(pathFrom, pathTo);
+});
+
+// Copy client to gh-pages
+const pathFrom = path.join(projectDir, "client");
+const pathTo = path.join(projectDir, "gh-pages");
+copyRecursiveSync(pathFrom, pathTo);
+
+// https://stackoverflow.com/a/22185855/14887710
+function copyRecursiveSync (src, dest) {
+    const exists = fs.existsSync(src);
+    const stats = exists && fs.statSync(src);
+    const isDirectory = exists && stats.isDirectory();
+    if (isDirectory) {
+        fs.mkdirSync(dest, { recursive: true });
+        fs.readdirSync(src).forEach((childItemName) => {
+            copyRecursiveSync(path.join(src, childItemName), path.join(dest, childItemName));
+        });
+    } else {
+        fs.copyFileSync(src, dest);
+    }
+}
--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 import { startServer } from "@h0rn0chse/socket-server";
 
 startServer({
-    publicPaths: [["/", "/"]]
+    publicPaths: [[
+        "/demo", "/"
+    ], [
+        "/dist", "/libs/dark-mode-toggle"
+    ], [
+        "/node_modules/lottie-web/build/", "/libs/lottie-web"
+    ], [
+        "/node_modules/wc-github-corners/dist", "/libs/wc-github-corners"
+    ]]
 });
