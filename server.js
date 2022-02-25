baf0ef45a5b42bebe637519f3747db038a16dd7a
H0rn0chse <h0rn0chse@gmx.de>
2022-02-25 23:51:36 +0100

    Updating server to use gh-pages-dependencies.json

--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 import { startServer } from "@h0rn0chse/socket-server";
+import fs from "fs";
+
+const json = fs.readFileSync("./gh-pages-dependencies.json")
+const dependencies = JSON.parse(json);
 
 startServer({
-    publicPaths: [[
-        "/demo", "/"
-    ], [
-        "/dist", "/libs/dark-mode-toggle"
-    ], [
-        "/node_modules/lottie-web/build/", "/libs/lottie-web"
-    ], [
-        "/node_modules/wc-github-corners/dist", "/libs/wc-github-corners"
-    ]]
+    publicPaths: dependencies
 });
